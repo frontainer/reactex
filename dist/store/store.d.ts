e9120543e3918e14e78fb1c5afcf908af6386a6d
frontainer <frontainer@gmail.com>
2020-01-17 10:11:47 +0900

    support next.js

--- dist/store/store.d.ts
+++ dist/store/store.d.ts
@@ -4,5 +4,6 @@ export declare class ReduxStore<S> {
     private _combineReducers;
     readonly store: Store<S, AnyAction>;
     readonly combineReducers: Reducer<S>;
+    private _createComposer;
     constructor(reducers: ReducersMapObject<S, AnyAction>, middlewares?: Middleware<any, S, any>[], debug?: boolean);
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -9,6 +9,8 @@ import {
   Store
 } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+const __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ = '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'
+const __NEXT_REDUX_STORE__ = '__NEXT_REDUX_STORE__'
 
 export class ReduxStore<S> {
   private _store: Store<S, AnyAction>
@@ -19,9 +21,20 @@ export class ReduxStore<S> {
   get combineReducers(): Reducer<S> {
     return this._combineReducers
   }
+  private _createComposer(debug: boolean) {
+    if (!debug || typeof window === 'undefined') {
+      return compose
+    }
+    if (!window.hasOwnProperty(__NEXT_REDUX_STORE__)) {
+      (window as any)[__NEXT_REDUX_STORE__] = compose
+    }
+    if (!window.hasOwnProperty(__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)) {
+      (window as any)[__REDUX_DEVTOOLS_EXTENSION_COMPOSE__] = compose
+    }
+    return compose
+  }
   constructor(reducers: ReducersMapObject<S, AnyAction>, middlewares: Middleware<any, S, any>[] = [], debug: boolean = false) {
-    const composeEnhancers = debug && (window as any).hasOwnProperty('__REDUX_DEVTOOLS_EXTENSION_COMPOSE__') ?
-      (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+    const composeEnhancers = this._createComposer(debug)
     this._combineReducers = combineReducers(reducers)
     this._store = _createStore<S, AnyAction, any, any>(
       this._combineReducers,
