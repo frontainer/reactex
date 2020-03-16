1a8fca4ef683e7111e520e7670728753d85d6106
frontainer <frontainer@gmail.com>
2020-03-16 14:08:50 +0900

    update dist

--- dist/store/store.d.ts
+++ dist/store/store.d.ts
@@ -4,5 +4,6 @@ export declare class ReduxStore<S> {
     private _combineReducers;
     get store(): Store<S, AnyAction>;
     get combineReducers(): Reducer<S>;
+    private _createComposer;
     constructor(reducers: ReducersMapObject<S, AnyAction>, middlewares?: Middleware<any, S, any>[], debug?: boolean);
 }
